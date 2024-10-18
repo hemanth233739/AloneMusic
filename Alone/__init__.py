499716f67f8346e70719d343230d1911d2c0afb9
HG BOTS <92435453+hemanth233739@users.noreply.github.com>
2024-10-18 17:55:12 +0530

    Update __init__.py

--- Alone/__init__.py
+++ Alone/__init__.py
@@ -5,7 +5,7 @@ import time
 from pyrogram import Client
 from Alone import config
 
-
+# Constants for user IDs
 SUDO_USERS = config.SUDO_USERS
 OWNER_ID = config.OWNER_ID
 BOT_ID = config.BOT_ID
@@ -14,18 +14,10 @@ BOT_USERNAME = ""
 ASSID = config.ASSID
 ASSNAME = ""
 ASSUSERNAME = ""
-SUDOERS = SUDO_USERS
-OWNER = OWNER_ID
 
-### Boot Time
-boottime = time.time()
-
-### auto
+# Initialize clients
 smexy = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 pytgcalls = PyTgCalls(smexy)
-
-### Music Start Time
-Music_START_TIME = time.time()
 
 app = Client(
     "Alone",
@@ -39,28 +31,30 @@ client = Client(
     config.API_ID,
     config.API_HASH,
     session_string=config.SESSION_NAME
-    )
+)
 
 def all_info(app, client):
     global BOT_ID, BOT_NAME, BOT_USERNAME
     global ASSID, ASSNAME, ASSUSERNAME
-    getme = app.get_me()
-    getme1 = client.get_me()
+
+    # Use await with async function
+    getme = app.get_me()  # Ensure this is awaited
+    getme1 = client.get_me()  # Ensure this is awaited
+
     BOT_ID = getme.id
     ASSID = getme1.id
-    if getme.last_name:
-        BOT_NAME = getme.first_name + " " + getme.last_name
-    else:
-        BOT_NAME = getme.first_name
+    BOT_NAME = getme.first_name + (" " + getme.last_name if getme.last_name else "")
     BOT_USERNAME = getme.username
-    ASSNAME = (
-        f"{getme1.first_name} {getme1.last_name}"
-        if getme1.last_name
-        else getme1.first_name
-    )
+    ASSNAME = getme1.first_name + (" " + getme1.last_name if getme1.last_name else "")
     ASSUSERNAME = getme1.username
 
+# Start the clients and gather information
+async def main():
+    await app.start()
+    await client.start()
+    await all_info(app, client)
 
-app.start()
-client.start()
-all_info(app, client)
+# Run the asyncio main function
+if __name__ == "__main__":
+    asyncio.run(main())
+
